7656438eb26843778a4aa7531d8e9e7159b8529a
Hannah Mungamuri <hannahmoses612@gmail.com>
2017-02-01 01:14:04 -0600

    displays correctinfofolder instead of boxes:

--- main.py
+++ main.py
@@ -15,22 +15,32 @@ import cgi
 #     warn = "<textarea style='color:red' name ='userwarning'>" + warning +"</textarea>"
 #     gooduser = valid_info("user")
 #     return text_label + textarea + warn
-warning=" "
+warned=" "
 def valid_info(ipgiven):
     x= ipgiven
     for i in range(len(ipgiven)):
         if x[i] != " " :
             return True
+    warned = "This is not a valid username."
+    return False
+def getwarned(warned):
+    if warned == "This is not a valid username":
+        return False
+    elif warned == " ":
+        return True
 def display_Username(fieldname):
     textarea_label = "<label style='display:in-line block;width:150px'>"+fieldname+" </label>"
     if fieldname == "Username" :
         textarea = "<input type='text' name ='ipgiven'/>"
         if valid_info('ipgiven'):
             validuser='ipgiven'
-            return post(self,correctinfoheader)
+            bighead="correctinfoheader"
+            return bighead
         else:
-            warned = " This is not a valid username."
-            return post(self,errorinfoheader)
+            invaliduser = ipgiven
+#            warned = " This is not a valid username."
+            bighead = "errorinfoheader"
+            return bighead
     elif fieldname == "Password" :
         textarea = "<input type='text' name ='ipgiven'/>"
         goodpassword = valid_info("ipgiven")
@@ -40,28 +50,42 @@ def display_Username(fieldname):
         else:
             warned = "warning appears here."
             return textarea_label+textarea+warned
+
 def build_signup():
     body = "<body style='background-color:white'>.<br><br></body>"
     submit = "<input type='submit' value='Submit Query'/>"
     form = ("<form style='color=pink' method='post'>" +
            display_Username("Username")+"<br><br>"+submit+
           "</form>")
+    outform = build_output(display_Username("Username"))
     return body + form
+
+def build_output(bighead):
+    textarea_label = "<label style='display:in-line block;width:150px'>"+bighead+" </label>"
+    body = "<body style='background-color:white'>.<br><br></body>"
+    submit = "<input type='submit' value='Submit Query'/>"
+    outform = ("<form style='color=pink' method='post'>" +
+              display_Username("Username")+"<br><br>"+ warned +submit+
+              "</form>")
+#     outform = build_output(display_Username("Username"))
+    return body + outform
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         header = "<h1 style='font-family: 'Times New Roman';color:black' > Signup</h1>"
         content = build_signup()
         self.response.out.write(header + content)
-    def post(self,bighead):
-        user = self.request.get("ipgiven")
-        correctinfoheader = "<h1 style='font-family: 'Times New Roman';color:black' > Welcome, " + user +"! </h1>"
-        if bighead == "correctinfoheader" :
+    def post(self):
+        if getwarned(warned) == True:
+#            fieldname == "Username" and bighead == "correctinfoheader":
+            user = self.request.get("ipgiven")
+            correctinfoheader = "<h1 style='font-family: 'Times New Roman';color:black' > Welcome, " + user +"! </h1>"
             content= build_signup()
             self.response.out.write(correctinfoheader+content)
-        elif bighead == "errorinfoheader":
-            warning = " This is not a valid username"#getwarn(fieldname)
-            content = build_signup()
-            self.request.out.write(errorinfoheader+content+warning)
+        elif  getwarned(warned) == "This is not a valid user name.":
+#            fieldname == "Username" and bighead == "errorinfoheader":
+            warningnow = " This is not a valid username"
+            content = output()
+            self.request.out.write(errorinfoheader+content+warningnow)
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
     ], debug=True)
