7398178eb385b6419789cd42846c0d2ed34f4800
Hannah Mungamuri <hannahmoses612@gmail.com>
2017-02-22 19:29:13 -0600

    started using templates for form in shopping_list.html

--- main.py
+++ main.py
@@ -1,91 +1,63 @@
+#TEMPLATES  feb 20 2017,Mon 6:56pm PRESIDENT'S DAY 4430GSWAY Matt6:1verse
+import os
+import webapp2#rgb(0,234,200) also use rgb(0,234,222,
+import jinja2
+from google.appengine.ext import db
 
-#2017Jan31 rough play of user sign up
-import webapp2
-import cgi
-# def getwarn():
-#     if goodusername:
-#         return textarea_label + textarea
-#     else:
-#         warned = "warning appears here."
-#         return textarea_label+textarea+warned
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-# def display_Userwarned(warning):
-#     textarea_label = "<label style='display:in-line block;width:150px'>Username</label>"
-#     textarea = "<textarea name ='userip'>" +textarea_content +"</textarea>"
-#     warn = "<textarea style='color:red' name ='userwarning'>" + warning +"</textarea>"
-#     gooduser = valid_info("user")
-#     return text_label + textarea + warn
-warned=" "
-def valid_info(ipgiven):
-    x= ipgiven
-    for i in range(len(ipgiven)):
-        if x[i] != " " :
-            return True
-    warned = "This is not a valid username."
-    return False
-def getwarned(warned):
-    if warned == "This is not a valid username":
-        return False
-    elif warned == " ":
-        return True
-def display_Username(fieldname):
-    textarea_label = "<label style='display:in-line block;width:150px'>"+fieldname+" </label>"
-    if fieldname == "Username" :
-        textarea = "<input type='text' name ='ipgiven'/>"
-        if valid_info('ipgiven'):
-            validuser='ipgiven'
-            bighead="correctinfoheader"
-            return bighead
-        else:
-            invaliduser = ipgiven
-#            warned = " This is not a valid username."
-            bighead = "errorinfoheader"
-            return bighead
-    elif fieldname == "Password" :
-        textarea = "<input type='text' name ='ipgiven'/>"
-        goodpassword = valid_info("ipgiven")
-        if goodpassword:
-            pwd='ipgiven'
-            return textarea_label + textarea
-        else:
-            warned = "warning appears here."
-            return textarea_label+textarea+warned
+# form_html = """
+#         # <form >
+#         #    <body style='background-color:rgb(0,234,200)' >
+#         #    <h2 style='color:rgb(234,13,156)'> Add a food </h2>
+#         #    <input type = "text"  name="food" ><br><br>
+#         #    %s
+#         #    <button> Add food </button>
+#         #    </body>
+#         # </form>
+# """
+hidden_html = """
+<input type ="hidden" name="food" value="%s"
+"""
+item_html = "<li>%s</li>"#this will be a list item, that goes inside the shopping_list_html
+shopping_list_html = """
+<br> <br>
+<h2 style='color:white;background-color:rgb(234,13,156)'>Shopping List</h2>
+<ul>
+%s
+</ul>
+"""
+class Handler (webapp2.RequestHandler):
+    def write(self,*a,**kw):
+        self.response.out.write(*a,**kw)
 
-def build_signup():
-    body = "<body style='background-color:white'>.<br><br></body>"
-    submit = "<input type='submit' value='Submit Query'/>"
-    form = ("<form style='color=pink' method='post'>" +
-           display_Username("Username")+"<br><br>"+submit+
-          "</form>")
-    outform = build_output(display_Username("Username"))
-    return body + form
+    def render_str(self,template,**params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-def build_output(bighead):
-    textarea_label = "<label style='display:in-line block;width:150px'>"+bighead+" </label>"
-    body = "<body style='background-color:white'>.<br><br></body>"
-    submit = "<input type='submit' value='Submit Query'/>"
-    outform = ("<form style='color=pink' method='post'>" +
-              display_Username("Username")+"<br><br>"+ warned +submit+
-              "</form>")
-#     outform = build_output(display_Username("Username"))
-    return body + outform
-class MainHandler(webapp2.RequestHandler):
+    def render (self,template,**kw):
+        self.write(self.render_str(template,**kw))
+
+class MainPage(Handler):
     def get(self):
-        header = "<h1 style='font-family: 'Times New Roman';color:black' > Signup</h1>"
-        content = build_signup()
-        self.response.out.write(header + content)
-    def post(self):
-        if getwarned(warned) == True:
-#            fieldname == "Username" and bighead == "correctinfoheader":
-            user = self.request.get("ipgiven")
-            correctinfoheader = "<h1 style='font-family: 'Times New Roman';color:black' > Welcome, " + user +"! </h1>"
-            content= build_signup()
-            self.response.out.write(correctinfoheader+content)
-        elif  getwarned(warned) == "This is not a valid user name.":
-#            fieldname == "Username" and bighead == "errorinfoheader":
-            warningnow = " This is not a valid username"
-            content = output()
-            self.request.out.write(errorinfoheader+content+warningnow)
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-    ], debug=True)
+        self.render("shopping_list.html")
+
+        ##Using function named render instead of the code below
+        # a="<h4 style='color:white;background-color:rgb(234,13,156);text-align:center''>Hello, Udacity !!!!<h4>"
+        # self.write(a)
+        # output = form_html
+        # output_hidden =""#output_html will hold the content that is going # to be stuffed into %sFOR NOW ,it is an empty string ""
+        # items = self.request.get_all("food")#gets all GET parameters and  #POST parameters of the same name "food"in the URL
+        # if items:
+        #     output_items = ""
+        #     for item in items :
+        #         output_hidden+= hidden_html % item#hidden_html refers to the HTML string %s that is going to  hold the hiden value.For every item in items, add to the string #output_hidden, the value os hidden_html, substituting the food name in item
+        #         output_items+= item_html % item
+        #     output_shopping = shopping_list_html % output_items
+        #     output += output_shopping
+        # output = output % output_hidden
+        # self.write(output)
+app = webapp2.WSGIApplication([('/',MainPage),
+                                ],
+                                debug=True)
